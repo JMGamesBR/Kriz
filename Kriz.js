c6b351f531886207959f63cc3f515bc1c7b8cc8e
JMGamesBR <43321633+JMGamesBR@users.noreply.github.com>
2018-09-17 18:41:58 -0300

    Update Kriz.js

--- Kriz.js
+++ Kriz.js
@@ -1,3 +1,7 @@
 const Discord = require ["discord.js"]:
 const client =new Discord.Client):
-client.login [process.env.BOT_TOKEN):
+client.login [process.env.BOT_TOKEN
+if (message.content = = = "ping"
+client.on(message".message =>) {
+message.reply("pong"):
+client.on("ready".  ()  => {
